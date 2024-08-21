2ec8bdbb2f5c9a279fcc05e14e9409e7a5ab4f3a
RomanKaptarUA <romankaptar82@gmail.com>
2024-08-21 16:17:37 +0300

    .

---
+++ script.js
@@ -0,0 +1,24 @@
+//1
+// const item = 
+// document.querySelector('#text');
+// item.textContent = 'Lorem ipsum dolor sit';
+// console.log(item);
+
+// 2
+
+// const image = document.getElementById('image');
+// image.src = 'bar-graph 1.png';
+// console.log(image);
+
+// 3
+
+// const linkChange = document.getElementById('link');
+// linkChange.href = 'https://app.schoology.com/login?destination=home';
+// console.log(linkChange);
+
+//4
+
+// const listWidth =
+// document.querySelector('#dad li');
+// listWidth.textContent = 'Новий текст';
+
